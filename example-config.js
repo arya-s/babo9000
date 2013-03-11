ffe3e8ec72c4d4420e1a5b25c2b14648196ed2e9
babofitos <tehchoi@gmail.com>
2013-03-11 03:24:36 -0400

    add example config.js

---
+++ example-config.js
@@ -0,0 +1,9 @@
+var config = {
+  channel: '#yourchannel'
+  , secret: ' secret'
+  //event content is ajaxed so must use ?content_only for the times and dates
+  , group: 'http://steamcommunity.com/groups/girlbloggers/events?content_only=true'
+  , trigger: '.'
+}
+
+module.exports = config

249d7e627eec24fbd53fac1b7fa68f75766ffeb2
babofitos <tehchoi@gmail.com>
2013-03-17 01:15:26 -0400

    separate group url in config

--- example-config.js
+++ example-config.js
@@ -1,11 +1,12 @@
 var config = {
   channel: '#yourchannel'
   , secret: ' secret'
-  //event content is ajaxed so must use ?content_only for the times and dates
-  , group: 'http://steamcommunity.com/groups/girlbloggers/events?content_only=true'
+  , group: 'http://steamcommunity.com/groups/girlbloggers/'
   , trigger: '.'
   //utc offset for event times
   , timezoneoffset: '-14400'
 }
 
+//event content is ajaxed so must use ?content_only for the times and dates
+config.group += 'events?content_only=true'
 module.exports = config

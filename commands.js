6c14b01b3e35a925c013e1efc02b4f32622ad2b4
babofitos <tehchoi@gmail.com>
2013-03-17 20:26:23 -0400

    add 8ball command

--- commands.js
+++ commands.js
@@ -26,9 +26,39 @@ function commands (command, nick, to, text, client) {
       break
     case 'rtd':
       rollDice(nick, to, client)
+    case '8ball':
+      eightball(nick, to, client)
     default:
       break
   }
+}
+
+function eightball (nick, to, client) {
+  var random = Math.floor(Math.random() * 21)
+    , answers = [
+      'it is certain'
+      , 'it is decidedly so'
+      , 'without a doubt'
+      , 'yes - definitely'
+      , 'you may rely on it'
+      , 'as i see it, yes'
+      , 'most likely'
+      , 'outlook good'
+      , 'yes'
+      , 'signs point to yes'
+      , 'reply hazy, try again'
+      , 'ask again later'
+      , 'better not tell you now'
+      , 'cannot predict now'
+      , 'concentrate and ask again'
+      , 'don\'t count on it'
+      , 'my reply is no'
+      , 'my sources say no'
+      , 'outlook not so good'
+      , 'very doubtful'
+    ]
+  
+  client.say(to, answers[random])
 }
 
 function rollDice (nick, to, client) {

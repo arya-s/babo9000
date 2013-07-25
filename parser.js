c8f147ecb40ee9b6649a68ac338be83c8ac0c4ee
babofitos <tehchoi@gmail.com>
2013-07-25 00:28:15 -0400

    fix indentation

--- parser.js
+++ parser.js
@@ -1,33 +1,33 @@
 var FeedParser = require('feedparser')
-	, request = require('request')
+  , request = require('request')
 
 var filter = ['[utw-mazui] railgun', '[commie] love lab']
-	, len = filter.length
+  , len = filter.length
 
 module.exports = function(channel, client) {
   request('https://www.tokyotosho.info/rss.php?filter=1')
-	.pipe(new FeedParser())
-	.on('error', function(err) {
-	  console.log(err)
-	})
-	.on('readable', function() {
-	  var stream = this
-	  	, item = stream.read()
+  .pipe(new FeedParser())
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .on('readable', function() {
+    var stream = this
+      , item = stream.read()
 
-	  for (var i=0;i<len;i++) {
-		  //read filters from somewhere, escape any regex specials
-	  	var escapeBrackets = filter[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
-	  		, currentFilter = escapeBrackets.split(' ')
-	    	, re = '(?=.*'
+    for (var i=0;i<len;i++) {
+      //read filters from somewhere, escape any regex specials
+      var escapeBrackets = filter[i].replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
+        , currentFilter = escapeBrackets.split(' ')
+        , re = '(?=.*'
 
-	    re += currentFilter.join(')(?=.*')
-	    re += ').+'
-	    var reobj = new RegExp(re, "i")
-	    console.log(reobj, item.title)
-	    if (item.title.match(reobj) != null) {
-	    	//say in irc
-	    	client.say(channel, item.title + ' ' + item.link)
-	    }
-  	}
-	})
+      re += currentFilter.join(')(?=.*')
+      re += ').+'
+      var reobj = new RegExp(re, "i")
+      console.log(reobj, item.title)
+      if (item.title.match(reobj) != null) {
+        //say in irc
+        client.say(channel, item.title + ' ' + item.link)
+      }
+    }
+  })
 }

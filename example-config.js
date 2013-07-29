ca4dad2d295e4a74994e874bb47836c3a4f338ac
babofitos <tehchoi@gmail.com>
2013-07-29 02:44:26 -0400

    update example config

--- example-config.js
+++ example-config.js
@@ -1,12 +1,9 @@
 var config = {
   channel: '#yourchannel'
   , secret: 'secret'
-  , group: 'http://steamcommunity.com/groups/girlbloggers/'
   , trigger: '.'
-  //utc offset for event times
-  , timezoneoffset: '-14400'
+  , username: ''
+  , joinMsg: 'hello world'
 }
 
-//event content is ajaxed so must use ?content_only for the times and dates
-config.group += 'events?content_only=true'
 module.exports = config

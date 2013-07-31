82df097cbc90d7355c49e3358506913559be40ed
babofitos <tehchoi@gmail.com>
2013-07-31 03:08:41 -0400

    refactor

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-require('./lib/db.js')(function(db) {
+//connect to db and get instance before anything else
+//this is ugly but i'm not sure of a better way to do it
+
+require('./lib/db.js')(app)
+
+function app(db) {
   require('./server.js')
   require('./lib/irc.js')(db)
-})
+}

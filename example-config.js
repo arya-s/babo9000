359982d65da2bc6cdbe9d2aec6e14c5bb44dc939
babofitos <tehchoi@gmail.com>
2014-01-15 23:14:32 -0500

    development and production in config

--- example-config.js
+++ example-config.js
@@ -1,14 +1,28 @@
 var config = {
-  channel: '#yourchannel'
-  , secret: 'secret'
-  , trigger: '.'
-  , username: ''
-  , joinMsg: ['hello world']
-  , website: 'http://example.org'
-  , steam_username: ''
-  , steam_password: ''
-  , steam_chatroom_id: ''
-  , master: ''
+  development: {
+    channel: '#yourchannel'
+    , secret: 'secret'
+    , trigger: '.'
+    , username: ''
+    , joinMsg: ['hello world']
+    , website: 'http://example.org'
+    , steam_username: ''
+    , steam_password: ''
+    , steam_chatroom_id: ''
+    , master: ''
+  },
+  production: {
+    channel: '#yourchannel'
+    , secret: 'secret'
+    , trigger: '.'
+    , username: ''
+    , joinMsg: ['hello world']
+    , website: 'http://example.org'
+    , steam_username: ''
+    , steam_password: ''
+    , steam_chatroom_id: ''
+    , master: ''
+  }
 }
 
 module.exports = config
